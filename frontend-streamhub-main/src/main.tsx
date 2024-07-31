115ae43651177952f92da93bcd1015fb0a57d262
yongchean foo <yongchean.foo@fdmgroup.com>
2024-08-01 07:15:43 +0800

    Restructure1

--- frontend-streamhub-main/src/main.tsx
+++ frontend-streamhub-main/src/main.tsx
@@ -38,6 +38,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RootRun></RootRun>
   </React.StrictMode>,
 );
---
+++ frontend-streamhub-main/src/pages/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import {Button} from "@/components/shadcn/ui/button.tsx";
+import {Link} from "react-router-dom";
+
+
+const Navbar = () => {
+
+    return (
+        <div className="h-[12.5vh] bg-black flex justify-center items-center">
+
+            {/*<Button variant="secondary" asChild>*/}
+            {/*    /!*Button*!/*/}
+            {/*    <Link to={`/`}>Homepage</Link>*/}
+            {/*</Button>*/}
+            <Button variant="secondary" asChild>
+                <Link to={`/watch/one-piece`}>Login</Link>
+            </Button>
+
+        </div>
+    );
+};
+
+export default Navbar;
