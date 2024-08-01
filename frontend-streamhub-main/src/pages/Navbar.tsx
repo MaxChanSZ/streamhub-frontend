fe402b2d978129a55c82932df33734c9d152c345
Timothy Chai <timothy.chai@fdmgroup.com>
2024-08-01 10:29:52 +0800

    Clean up merge conflicts

--- frontend-streamhub-main/src/pages/Navbar.tsx
+++ frontend-streamhub-main/src/pages/Navbar.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
-import {Button} from "@/components/shadcn/ui/button.tsx";
-import {Link} from "react-router-dom";
-
+import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/shadcn/ui/navigation-menu.tsx";
+import { Button } from "@/components/shadcn/ui/button.tsx";
+import { Link } from "react-router-dom";
+import logo from "/streamhub-logo.svg";
 
 const Navbar = () => {
+  const buttonTextFormat = "text-base";
 
-    return (
-        <div className="h-[12.5vh] bg-black flex justify-center items-center">
-
-            {/*<Button variant="secondary" asChild>*/}
-            {/*    /!*Button*!/*/}
-            {/*    <Link to={`/`}>Homepage</Link>*/}
-            {/*</Button>*/}
-            <Button variant="secondary" asChild>
-                <Link to={`/watch/one-piece`}>Test</Link>
-            </Button>
-
+  return (
+    <div className="h-[12.5vh] bg-black flex">
+      <NavigationMenu className="font-alatsi text-white">
+        <div className="pl-20 pr-0 max-w-64">
+          <Link to={`/`}>
+            <img src={logo} alt="StreamHub Logo" />
+          </Link>
         </div>
-    );
+
+        <NavigationMenuList className="px-2 py-4 flex justify-center">
+          <NavigationMenuItem>
+            <Button variant="ghost" className={buttonTextFormat} asChild>
+              <Link to={`/watch-party/1456`}>Join a Watch Party</Link>
+            </Button>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Button variant="ghost" className={buttonTextFormat} asChild>
+              <Link to={`/update-profile`}>Update Profile</Link>
+            </Button>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
 };
 
 export default Navbar;
