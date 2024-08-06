0658c0dca50e4c7642d3207ddbfc8b54c6069389
yongchean foo <yongchean.foo@fdmgroup.com>
2024-08-06 15:58:49 +0800

    profile icon

--- frontend-streamhub-main/src/components/Navbar.tsx
+++ frontend-streamhub-main/src/components/Navbar.tsx
@@ -11,6 +11,8 @@ import {
 } from "@/components/shadcn/ui/navigation-menu.tsx";
 import { Button } from "@/components/shadcn/ui/button.tsx";
 import { Link } from "react-router-dom";
+import profileIcon from "/profileicon.svg";
+import profileIconBg from "/profileiconbg.svg";
 import logo from "/streamhub-logo.svg";
 
 const Navbar = () => {
@@ -25,26 +27,33 @@ const Navbar = () => {
           </Link>
         </div>
 
-        <NavigationMenuList className="py-4 flex justify-center">
-          <NavigationMenuItem>
-            <Button variant="ghost" className={buttonTextFormat} asChild>
-              <Link to={`/watch-party/1456`}>Create a Watch Party</Link>
-            </Button>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Button variant="ghost" className={buttonTextFormat} asChild>
-              <Link to={`/update-profile`}>Update Profile</Link>
-            </Button>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Button variant="ghost" className={buttonTextFormat} asChild>
-              <Link to={`/contact`}>Contact</Link>
-            </Button>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </div>
-  );
+          <NavigationMenuList className="py-4 flex justify-center">
+            <NavigationMenuItem>
+              <Button variant="ghost" className={buttonTextFormat} asChild>
+                <Link to={`/watch-party/1456`}>Create a Watch Party</Link>
+              </Button>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Button variant="ghost" className={buttonTextFormat} asChild>
+                <Link to={`/update-profile`}>Update Profile</Link>
+              </Button>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Button variant="ghost" className={buttonTextFormat} asChild>
+                <Link to={`/contact`}>Contact</Link>
+              </Button>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <div className="ml-auto relative flex items-center mr-20">
+          <div className="relative w-12 h-12">
+            <img src={profileIcon} alt="Profile Icon Background" className="absolute top-0 left-0 w-12 h-12"/>
+            <img src={profileIconBg} alt="Profile Icon" className="absolute top-0 left-0 w-12 h-12"/>
+          </div>
+        </div>
+      </div>
+)
+  ;
 };
 
 export default Navbar;
