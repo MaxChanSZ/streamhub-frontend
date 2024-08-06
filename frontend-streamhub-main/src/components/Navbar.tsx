5a95f750cdfb42d6bf47561c479d8a4774cc7009
yongchean foo <yongchean.foo@fdmgroup.com>
2024-08-06 16:29:54 +0800

    add searchbar

--- frontend-streamhub-main/src/components/Navbar.tsx
+++ frontend-streamhub-main/src/components/Navbar.tsx
@@ -10,22 +10,24 @@ import {
   NavigationMenuViewport,
 } from "@/components/shadcn/ui/navigation-menu.tsx";
 import { Button } from "@/components/shadcn/ui/button.tsx";
+import { Input } from "@/components/shadcn/ui/input.tsx"
 import { Link } from "react-router-dom";
 import profileIcon from "/profileicon.svg";
 import profileIconBg from "/profileiconbg.svg";
 import logo from "/streamhub-logo.svg";
 
+
 const Navbar = () => {
   const buttonTextFormat = "text-base mx-4";
 
   return (
-    <div className="h-[12.5vh] bg-black flex">
-      <NavigationMenu className="font-alatsi text-white">
-        <div className="pl-20 pr-0 max-w-64 mr-4">
-          <Link to={`/`}>
-            <img src={logo} alt="StreamHub Logo" />
-          </Link>
-        </div>
+      <div className="h-[12.5vh] bg-black flex">
+        <NavigationMenu className="font-alatsi text-white">
+          <div className="pl-20 pr-0 max-w-64 mr-4">
+            <Link to={`/`}>
+              <img src={logo} alt="StreamHub Logo"/>
+            </Link>
+          </div>
 
           <NavigationMenuList className="py-4 flex justify-center">
             <NavigationMenuItem>
@@ -45,10 +47,20 @@ const Navbar = () => {
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
-        <div className="ml-auto relative flex items-center mr-20">
-          <div className="relative w-12 h-12">
-            <img src={profileIcon} alt="Profile Icon Background" className="absolute top-0 left-0 w-12 h-12"/>
-            <img src={profileIconBg} alt="Profile Icon" className="absolute top-0 left-0 w-12 h-12"/>
+
+        <div className="ml-auto flex items-center space-x-8">
+          <Input
+              type="search"
+              placeholder="Search"
+              className="w-full max-w-md bg-black text-white placeholder-white "
+              style={{ width: '300px' }} />
+          <div className="relative flex items-center pl-8 pr-20">
+            <div style={{width: '50px', height: '50px'}} className="relative">
+              <img src={profileIcon} alt="Profile Icon Background" style={{width: '50px', height: '50px'}}
+                   className="absolute top-0 left-0"/>
+              <img src={profileIconBg} alt="Profile Icon" style={{width: '50px', height: '50px'}}
+                   className="absolute top-0 left-0"/>
+            </div>
           </div>
         </div>
       </div>
--- frontend-streamhub-main/src/components/shadcn/ui/input.tsx
+++ frontend-streamhub-main/src/components/shadcn/ui/input.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "src/components/shadcn/ui/utils"
+import { cn } from "@/components/shadcn/ui/utils";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
