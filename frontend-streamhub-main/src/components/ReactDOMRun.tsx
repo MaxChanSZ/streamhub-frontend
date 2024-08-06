589d50f54fcd72a9ec88e6cd1122e49d340fe8d5
Tanay Rishi <Tanay.Rishi@fdmgroup.com>
2024-08-06 10:51:06 +0800

    Install necessary packages

--- frontend-streamhub-main/src/components/ReactDOMRun.tsx
+++ frontend-streamhub-main/src/components/ReactDOMRun.tsx
@@ -47,7 +47,7 @@ const router = createBrowserRouter([
       },
       {
         path: "watch/:seriesId/:episodeId",
-        element: <WatchPage />,
+        element: <WatchPage videoSource=""/>,
       },
       {
         path: "watch-party/:sessionId",
