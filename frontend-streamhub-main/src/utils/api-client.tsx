00e080225f7473e18272c55b5d8429231607190f
Timothy Chai <timothy.chai@fdmgroup.com>
2024-08-07 17:25:39 +0800

    Add delete method to call delete endpoint

--- frontend-streamhub-main/src/utils/api-client.tsx
+++ frontend-streamhub-main/src/utils/api-client.tsx
@@ -121,3 +121,30 @@ export const update = async (formData: UpdateFormData) => {
       throw new Error(error);
     });
 };
+
+export const deleteUser = async (id: number) => {
+  const response = await axios
+    .delete(`http://localhost:8080/account/api/delete/${id}`)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+      throw new Error(error);
+    });
+};
