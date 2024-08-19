594277c82785ffe928c5f3597fa1909dd7d6368e
Timothy Chai <timothy.chai@fdmgroup.com>
2024-08-19 15:45:57 +0800

    Add alert dialog and delete account button

---
+++ frontend-streamhub-main/src/components/DeleteAccountButton.tsx
@@ -0,0 +1,69 @@
+import { useAppContext } from "@/contexts/AppContext";
+import * as apiClient from "@/utils/api-client";
+import { useNavigate } from "react-router-dom";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/shadcn/ui/alert-dialog";
+import { Button } from "@/components/shadcn/ui/button";
+
+const DeleteAccountButton = () => {
+  const deleteButtonPress = () => {
+    const { setIsLoggedIn, user, setUser } = useAppContext();
+    const navigate = useNavigate();
+
+    if (user !== null) {
+      apiClient.deleteUser(user?.id);
+      setIsLoggedIn(false);
+      setUser(null);
+      navigate("/");
+    }
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger
+        className="bg-destructive text-destructive-foreground hover:bg-destructive/90 
+        inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium 
+        ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 
+        px-4 py-3"
+      >
+        Delete Account
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle className="font-bold">
+            Are you absolutely sure?
+          </AlertDialogTitle>
+          <AlertDialogDescription className="font-normal text-slate-800">
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => {
+              deleteButtonPress();
+            }}
+            className="bg-destructive text-destructive-foreground hover:bg-destructive/90 
+        inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium 
+        ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 
+        px-4 py-3"
+          >
+            Confirm
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default DeleteAccountButton;
