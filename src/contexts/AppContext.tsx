a33ce4c894ccf44d2dd9765690d426635cd48cf6
Timothy Chai <timothy.chai@fdmgroup.com>
2024-08-20 16:22:25 +0800

    Set custom hooks to persist login status into local storage (temp)

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { Dispatch, SetStateAction, useContext, useState } from "react";
 import { Toaster } from "@/components/shadcn/ui/toaster";
 import { User } from "@/utils/types";
 
@@ -10,6 +10,65 @@ type AppContext = {
 };
 
 const AppContext = React.createContext<AppContext | undefined>(undefined);
+
+function returnInitialState(storageKey: string) {
+  try {
+    // Get from local storage by key
+    const item = window.localStorage.getItem(storageKey);
+    // Parse stored json or if none return an empty object
+    return item ? JSON.parse(item) : {};
+  } catch (error) {
+    // If error also return an empty object
+    console.log(error);
+    return {};
+  }
+}
+
+function useLocalStorage(storageKey: string) {
+  const [storedValue, setStoredValue] = useState(
+    returnInitialState(storageKey)
+  );
+
+  const setValue = (value: any) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save to local storage
+      window.localStorage.setItem(storageKey, JSON.stringify(valueToStore));
+      // Save state
+      setStoredValue(valueToStore);
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
+function useLoginStatus(
+  status: string
+): [boolean, Dispatch<SetStateAction<boolean>>] {
+  const [statusString, setStatusString] = useLocalStorage(
+    returnInitialState(status)
+  );
+
+  const storedStatus = statusString === "true" ? true : false;
+
+  const setStatus = (value: SetStateAction<boolean>) => {
+    try {
+      if (typeof value === "function") {
+        setStatusString(value(storedStatus) ? "true" : "false");
+      } else {
+        setStatusString(value ? "true" : "false");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return [storedStatus, setStatus];
+}
 
 /**
  * AppContextProvider is a React Context Provider component that provides
@@ -26,7 +85,7 @@ export const AppContextProvider = ({
   children: React.ReactNode;
 }): JSX.Element => {
   // State variables to manage the login status and user information.
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [isLoggedIn, setIsLoggedIn] = useLoginStatus("loginStatus");
   const [user, setUser] = useState<User | null>(null);
 
   /**
