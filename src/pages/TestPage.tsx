6e049e8c624549be7e723b07980b981ff620570c
Timothy Chai <159506546+tchai17@users.noreply.github.com>
2024-08-22 11:02:36 +0800

    Clean up imports

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,8 +1,3 @@
-import { useState, useEffect } from "react";
-import SockJS from "sockjs-client";
-import { IFrame, Stomp } from "@stomp/stompjs";
-import { Button } from "@/components/shadcn/ui/button";
-import { useAppContext } from "@/contexts/AppContext";
 import LiveChat from "@/components/LiveChat";
 
 interface Message {
