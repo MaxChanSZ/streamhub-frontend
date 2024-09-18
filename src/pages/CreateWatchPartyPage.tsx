6838a42e70885eba3bdc0a7f38cc5a013e09a089
mayjiayi <mayjiayiw@gmail.com>
2024-09-18 11:27:02 +0800

    updated look of create watch party page

--- src/pages/CreateWatchPartyPage.tsx
+++ src/pages/CreateWatchPartyPage.tsx
@@ -50,21 +50,22 @@ const CreateWatchPartyPage = () => {
           Create Watch Party Page
         </h2>
         <form onSubmit={onFormSubmit}>
-          <label>
-            <h2>Watch Party Name</h2>
-            <input 
-              type="text" 
-              placeholder="Input Party Name" 
-              value={partyName}
-              onChange={(e) => setPartyName(e.target.value)}>
-              </input>
-          </label>
-          <Button 
-            type="submit" 
-            variant="secondary"
-            className="text-base mx-2 px-4 py-1 self-center font-alatsi"
-          >Submit
-          </Button>
+          <div className="sm:col-span-2">
+            <label className="font-alatsi block text-md font-medium leading-6 text-stone-50">Watch Party Name</label>
+            <div className="mt-2">
+              <input type="text" 
+                placeholder="Input Party Name eg. Horror Night" 
+                value={partyName}
+                onChange={(e) => setPartyName(e.target.value)}
+                className="font-alatsi w-1/2 mr-2 rounded-md border-0 py-2 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+              <Button 
+                type="submit" 
+                variant="secondary"
+                className="col-span-2 text-base px-4 py-1 self-center font-alatsi"
+                >Submit
+              </Button>
+            </div>
+          </div>
         </form>
       </div>
     );
