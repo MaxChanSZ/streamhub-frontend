dc9d6237ab5f2aa0229af4186183dbf6fd59a0ef
Timothy Chai <159506546+tchai17@users.noreply.github.com>
2024-09-19 14:44:04 +0800

    Remove redundant code leading to TestPage (not needed)

--- src/pages/TestPage.tsx
+++
@@ -1,9 +0,0 @@
-import LiveChat from "@/components/LiveChat";
-import { useState } from "react";
-
-const TestPage = () => {
-  const [roomID, setRoomID] = useState("");
-  return <LiveChat roomID={roomID} setRoomID={setRoomID} />;
-};
-
-export default TestPage;
