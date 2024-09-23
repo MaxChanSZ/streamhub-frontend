ffda37a506bebfd1fbbb4974dd9439a82d09c55a
Timothy Chai <159506546+tchai17@users.noreply.github.com>
2024-09-23 16:27:10 +0800

    Add timeout to emoji queue

--- src/components/EmojiOverlay.tsx
+++ src/components/EmojiOverlay.tsx
@@ -1,9 +1,10 @@
 import { EmojiConnection } from "@/utils/messaging-client";
 import { Emoji } from "./EmojiReaction";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const EmojiOverlay = ({ roomID }: { roomID: string }) => {
   const [emojiQueue, setEmojiQueue] = useState<Emoji[]>([]);
+  const DELAY = 500; // delay for timeout
 
   const disconnect = EmojiConnection({
     roomID,
@@ -14,11 +15,25 @@ const EmojiOverlay = ({ roomID }: { roomID: string }) => {
     },
   });
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (emojiQueue.length) {
+        const [first, ...rest] = emojiQueue;
+        setEmojiQueue(rest);
+      }
+    }, DELAY);
+    disconnect();
+    return () => clearTimeout(timer);
+  }, [emojiQueue]);
+
   return (
-    <div className="absolute inset-0 z-10">
+    <div
+    // className="absolute inset-0 z-10"
+    >
       {emojiQueue.map((emoji) => {
-        return <p key={emoji.ID}>{emoji.TYPE}</p>;
+        return <p key={emoji.ID}>{emoji.TYPE + "" + emoji.ID}</p>;
       })}
+      <p>{emojiQueue.length}</p>
     </div>
   );
 };
