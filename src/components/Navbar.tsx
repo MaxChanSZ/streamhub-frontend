f1e17cbe176f4e0beeffa7788d7d5389a0c60d99
yongchean foo <yongchean.foo@fdmgroup.com>
2024-09-30 12:05:35 +0800

    update Navbar WP to dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -95,18 +95,30 @@ const Navbar = () => {
             <div className="hidden lg:flex flex-grow justify-start">
                 <NavigationMenu className="font-alatsi text-white">
                     <NavigationMenuList className="py-4 flex items-center space-x-0">
-                        {/* Join Watch Party */}
-                        <NavigationMenuItem>
-                            <Button variant="ghost" className={buttonTextFormat} asChild>
-                                <Link to={`/join-watch-party`}>Join Watch Party</Link>
-                            </Button>
-                        </NavigationMenuItem>
 
-                        {/* Create Watch Party */}
-                        <NavigationMenuItem>
+                        {/*Watch Party*/}
+                        <NavigationMenuItem className="relative">
                             <Button variant="ghost" className={buttonTextFormat} asChild>
-                                <Link to={`/create-watch-party`}>Create Watch Party</Link>
+                                <NavigationMenuTrigger className="relative z-10">Watch Party</NavigationMenuTrigger>
                             </Button>
+                            <NavigationMenuContent className="bg-black w-fit">
+                                <ul className="flex flex-col items-center w-full p-2 space-y-2">
+                                    {wpCategories.map((category) => (
+                                        <li key={category.title} className="w-full">
+                                            <Button variant="ghost" className={categoryButtonFormat} asChild>
+                                                <Link
+                                                    to={category.href}
+                                                    className="text-white hover:underline block w-full py-2 px-4 text-center"
+                                                >
+                                                    <div>
+                                                        <h3 className="font-bold">{category.title}</h3>
+                                                    </div>
+                                                </Link>
+                                            </Button>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </NavigationMenuContent>
                         </NavigationMenuItem>
 
                         {/* Category */}
@@ -165,22 +177,6 @@ const Navbar = () => {
 
                         {/* Mobile Category Menu */}
                         <li>
-                            <Button
-                                variant="ghost"
-                                className={`${buttonTextFormat} text-white`}
-                                asChild
-                            >
-                                <Link
-                                    to={`/watch-party`}
-                                    className="block py-2"
-                                    onClick={() => setMenuOpen(false)} // Add this line
-                                >
-                                    Join Watch Party
-                                </Link>
-                            </Button>
-                        </li>
-
-                        <li>
                             <NavigationMenu>
                                 <NavigationMenuItem className="relative">
                                     <Button
@@ -214,20 +210,39 @@ const Navbar = () => {
                                         </ul>
                                     </NavigationMenuContent>
                                 </NavigationMenuItem>
-
-                                <Button
-                                    variant="ghost"
-                                    className={`${buttonTextFormat} text-white`}
-                                    asChild
-                                >
-                                    <Link
-                                        to={`/create-watch-party`}
-                                        className="block py-2"
-                                        onClick={() => setMenuOpen(false)}
+                                <NavigationMenuItem className="relative">
+                                    <Button
+                                        variant="ghost"
+                                        className={`${buttonTextFormat} text-white`}
+                                        asChild
                                     >
-                                        Create Watch Party
-                                    </Link>
-                                </Button>
+
+                                        <NavigationMenuTrigger
+                                            className="relative z-10">Watch Party</NavigationMenuTrigger>
+                                    </Button>
+                                    <NavigationMenuContent className="bg-black w-fit">
+                                        <ul className="flex flex-col items-center w-full gap-2 p-2">
+                                            {wpCategories.map((category) => (
+                                                <li key={category.title} className="w-full">
+                                                    <Button
+                                                        variant="ghost"
+                                                        className={`${categoryButtonFormat} text-white`}
+                                                        asChild
+                                                    >
+                                                        <Link
+                                                            to={category.href}
+                                                            className="text-white hover:underline block w-full py-2 px-4 text-center"
+                                                            onClick={() => setMenuOpen(false)} // Add this line
+                                                        >
+                                                            <h3 className="font-bold">{category.title}</h3>
+                                                        </Link>
+                                                    </Button>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </NavigationMenuContent>
+                                </NavigationMenuItem>
+
                                 <Button
                                     variant="ghost"
                                     className={`${buttonTextFormat} text-white`}
@@ -297,4 +312,9 @@ const categories: { title: string; href: string }[] = [
     { title: "Category 4", href: "/category4" },
     { title: "Category 5", href: "/category5" },
     { title: "Category 6", href: "/category6" },
+];
+
+const wpCategories: { title: string; href: string }[] = [
+    { title: "Join Watch Party", href: "/join-watch-party" },
+    { title: "Create Watch Party", href: "/create-watch-party" },
 ];
