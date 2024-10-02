d65062d6b7c94907eefcb3490a3e873c7ba43612
Timothy Chai <159506546+tchai17@users.noreply.github.com>
2024-10-02 17:18:31 +0800

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,6 @@
 import CarouselPage from "./CarouselPage";
-import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  const navigate = useNavigate();
-
   return (
     <div className="text-white justify-center items-center">
       <CarouselPage />
